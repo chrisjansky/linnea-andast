9397da5a2f5a1a9fa750d3757572f0bb3e526eea
Christian Jánský <chrisjansky@gmail.com>
2018-07-22 20:04:44 +0200

    Added .m-next as last slide in Index page

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -9,8 +9,9 @@ var chjSmoothScroll = require('./modules/laa-smoothscroll');
 
 var slideActiveClass = "Slide--is-active";
 var sliderEl = document.querySelector("[data-slider]");
+var sliderObj;
 if (sliderEl !== null) {
-  var sliderObj = new Flickity("[data-slider]", {
+  sliderObj = new Flickity("[data-slider]", {
     prevNextButtons: false,
     pageDots: false,
     setGallerySize: false,
@@ -33,6 +34,9 @@ if (sliderEl !== null) {
   });
 
   sliderObj.on( 'change' , function( index ) {
+    var isLast = index == sliderObj.cells.length - 1;
+    document.body.classList.toggle("Slider--end", isLast);
+
     if (prevSlide !== undefined) {
       prevSlide.getCellElements().forEach( function( cellElem ) {
         cellElem.classList.remove(slideActiveClass);
