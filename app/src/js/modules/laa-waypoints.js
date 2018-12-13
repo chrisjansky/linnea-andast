250bb299ab025628886d12c37e1004c56ebff918
Christian Jánský <chrisjansky@gmail.com>
2018-12-13 13:49:11 +0100

    Moved sliderEnd waypoint to .m-masthead

--- app/src/js/modules/laa-waypoints.js
+++ app/src/js/modules/laa-waypoints.js
@@ -13,6 +13,12 @@ module.exports = {
     new Waypoint({
       element: document.querySelector("[data-waypoint='Masthead']"),
       handler: function(direction) {
+        var sliderEnd = window.sliderObj.selectedIndex == window.sliderObj.cells.length - 1;
+        if (sliderEnd) {
+          document.body.classList.remove("Slider--end");
+          window.sliderObj.select(0); /* Go to first slide */
+        }
+
         if (direction == "up") {
           document.body.classList.remove("Hud--is-active");
         }
@@ -24,12 +30,6 @@ module.exports = {
       new Waypoint({
         element: sliderEl,
         handler: function(direction) {
-          var sliderEnd = window.sliderObj.selectedIndex == window.sliderObj.cells.length - 1;
-
-          if (sliderEnd) {
-            document.body.classList.toggle("Slider--end", direction == "down");
-          }
-
           if (direction == "down") {
             window.sliderObj.unpausePlayer();
           } else {
