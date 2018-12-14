4d01493163b38b06798eb2d0885b1911f02c1f00
Christian Jánský <chrisjansky@gmail.com>
2018-12-14 11:55:57 +0100

    Kirby Added error template

---
+++ kirby/site/templates/error.php
@@ -0,0 +1,11 @@
+<?php snippet('header') ?>
+
+<body class="Normal">
+
+  <div id="barba-wrapper">
+    <div class="barba-container">
+      <a class="m-error" href="<?= url($site) ?>">&larr; Error</a>
+    </div>
+  </div>
+
+<?php snippet('footer') ?>
